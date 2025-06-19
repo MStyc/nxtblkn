4bdf669048e3623b6e288eccf3b647bea29a46d1
MStyc <166760924+MStyc@users.noreply.github.com>
2025-06-19 23:24:17 +0200

    music-player.tsx

--- components/music-player.tsx
+++ components/music-player.tsx
@@ -23,7 +23,7 @@ const tracks: Track[] = [
     duration: "3:10",
     genre: "Folk Fusion",
     coverUrl: "/placeholder.svg?height=300&width=300",
-    audioUrl: "/music/Noćnirajj.mp3",
+    src: "/music/Noćnirajj.mp3",
   },
   {
     id: 2,
