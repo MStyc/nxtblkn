a86dd6ce56f55a01a09e13ada51ab7bdf49827b5
MStyc <166760924+MStyc@users.noreply.github.com>
2025-06-21 13:29:05 +0200

    navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -69,7 +69,7 @@ export function Navigation() {
                 >
                   {item.label}
                   {!scrolled && (
-                    <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full" />
+                    <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-black transition-all duration-300 group-hover:w-full" />
                   )}
                 </Link>
               ))}
